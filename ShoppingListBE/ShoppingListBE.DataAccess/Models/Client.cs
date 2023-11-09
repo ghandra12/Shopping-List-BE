ba8f785b5dcb5faec869b3a6c4b28bdd38da560c
DESKTOP-B6U7AQJ\Andra <gheorghe.andra.r2x@student.ucv.ro>
2023-11-09 21:05:20 +0200

    Initial commit + entity framework setup

---
+++ ShoppingListBE/ShoppingListBE.DataAccess/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingListBE.DataAccess.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public bool IsAdmin { get; set; }
+        public string PhoneNumber { get; set; } = "";
+
+    }
+}
---
+++ ShoppingListBE/ShoppingListBE.DataAccess/Models/ShoppingListDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingListBE.DataAccess.Models
+{
+    public class ShoppingListDBContext : DbContext
+    {
+        public ShoppingListDBContext(DbContextOptions<ShoppingListDBContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Client> Clients => Set<Client>();
+    }
+
+}
